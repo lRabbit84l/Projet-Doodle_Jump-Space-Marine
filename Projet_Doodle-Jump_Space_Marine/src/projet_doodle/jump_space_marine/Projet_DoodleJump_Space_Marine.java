db94e47da6588ddee76ac03bfb32eed944f6e5ae
raphv <raphv@10.2.60.203>
2026-03-10 16:36:55 +0100

    Test code

---
+++ Projet_Doodle-Jump_Space_Marine/src/projet_doodle/jump_space_marine/Projet_DoodleJump_Space_Marine.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projet_doodle.jump_space_marine;
+
+/**
+ *
+ * @author raphv
+ */
+public class Projet_DoodleJump_Space_Marine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      /*nfniudwng,nejntieshtizqntguyqwgn j'aime le choco
+    }
+    
+}
