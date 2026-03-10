982d3ace0eb85033e68124a0a21f46fcf8021b6e
lRabbit84l <jacobpujol84@gmail.com>
2026-03-10 17:18:56 +0100

    Ajout du projet NetBeans

---
+++ Projet_Doodle-Jump_Space_Marine/src/main/java/com/mycompany/projet_doodle/jump_space_marine/Projet_DoodleJump_Space_Marine.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projet_doodle.jump_space_marine;
+
+/**
+ *
+ * @author Jacob
+ */
+public class Projet_DoodleJump_Space_Marine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
