a2af45ff3d07f3fc95b30a159d96ba67ebff8eae
raphv <raphv@10.2.60.203>
2026-03-11 19:01:26 +0100

    Mise en place des bases de colision

---
+++ Projet_Doodle-Jump_Space_Marine/src/main/java/com/mycompany/projet_doodle/jump_space_marine/Plateforme.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projet_doodle.jump_space_marine;
+
+/**
+ *
+ * @author raphv
+ */
+public class Plateforme {
+     private int x, y, width = 40, height = 40;
+
+    public Plateforme(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+     
+}
